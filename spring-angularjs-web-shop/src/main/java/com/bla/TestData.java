7bc84c33b793a0de87042777258275a9dbe0fb68
novica <novica.parausic@hotmail.com>
2019-03-12 13:36:16 +0100

    Change TestData

--- spring-angularjs-web-shop/src/main/java/com/bla/TestData.java
+++ spring-angularjs-web-shop/src/main/java/com/bla/TestData.java
@@ -5,10 +5,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.bla.entity.Order;
 import com.bla.entity.Product;
 import com.bla.repository.ProductRepository;
-import com.bla.service.OrderService;
 
 @Component
 public class TestData {
@@ -16,18 +14,11 @@ public class TestData {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	@Autowired
-	private OrderService cartItemService;
-	
 	@PostConstruct
-	public void init(){
-		for(int i = 0; i<15; i++){
+	public void init() {
+		for(int i = 0; i<15; i++) {
 			Product prod = new Product("car" + i, 50 + i, 100 + i, true);
 			prod = productRepository.save(prod);
-		
-			Long l = new Long(i);
-			Order order = new Order(l);
-			cartItemService.save(order);
 		
 		}
 	}
